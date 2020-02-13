9c3f087f3fb3c1a134430de7f6ef6d1541f21dea
Mateusz Szklarek <mateusz.szklarek@gmail.com>
2020-02-13 15:12:18 +0100

    Refactor plugin.py

--- danger_py_jscpd/plugin.py
+++ danger_py_jscpd/plugin.py
@@ -9,12 +9,12 @@ class DangerJSCPD(DangerPlugin):
         if result.returncode == 1:
             self.fail("Could not find jscpd in current directory, pleas run command `npm install -g jscpd`")
         else:
-            subprocess.run(["jscpd", "."], capture_output=True, text=True)
-            try:
-                with open("/report/jscpd-report.json") as report:
-                    pass
-            except OSError:
-                self.fail("Could not find jscpd-report.json in /report directory")
+            self.run_jspcd()
 
-
-
+    def run_jspcd(self):
+        subprocess.run(["jscpd", "."], capture_output=True, text=True)
+        try:
+            with open("/report/jscpd-report.json") as report:
+                pass
+        except OSError:
+            self.fail("Could not find jscpd-report.json in /report directory")
