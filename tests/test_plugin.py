c7c19cc98d18f57103d5712801de09d97fefa5c4
Mateusz Szklarek <mateusz.szklarek@gmail.com>
2020-02-13 15:03:07 +0100

    Implement running jscpd command and reading jscpd-report.json

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -2,6 +2,7 @@ from unittest.mock import patch
 
 from danger_python.danger import Danger, Violation
 from danger_python.plugins import DangerPlugin
+from pyfakefs.fake_filesystem_unittest import Patcher
 from testfixtures.popen import MockPopen
 
 from danger_py_jscpd import DangerJSCPD
@@ -25,8 +26,38 @@ def test_plugin_does_fail_when_jscpd_not_installed(danger: Danger):
 def test_plugin_does_pass_when_jscpd_installed(danger: Danger):
     with patch("subprocess.Popen", new_callable=MockPopen) as popen:
         popen.set_command("which jscpd", returncode=0)
+        popen.set_command("jscpd .", returncode=0)
 
-        plugin = DangerJSCPD()
-        plugin.jscpd()
+        with Patcher() as patcher:
+            patcher.fs.create_file("/report/jscpd-report.json", contents="")
+            plugin = DangerJSCPD()
+            plugin.jscpd()
+
+    assert not danger.results.fails
+
+
+def test_plugin_could_not_find_jscpd_report(danger: Danger):
+    with patch("subprocess.Popen", new_callable=MockPopen) as popen:
+        popen.set_command("which jscpd", returncode=0)
+        popen.set_command("jscpd .", returncode=0)
+
+        with Patcher():
+            plugin = DangerJSCPD()
+            plugin.jscpd()
+
+    message = "Could not find jscpd-report.json in /report directory"
+    assert danger.results.fails == [Violation(message=message)]
+
+
+def test_plugin_founds_jscpd_report(danger: Danger):
+    with patch("subprocess.Popen", new_callable=MockPopen) as popen:
+        popen.set_command("which jscpd", returncode=0)
+        popen.set_command("jscpd .", returncode=0)
+
+        with open("tests/fixtures/jscpd-report.json") as report:
+            with Patcher() as patcher:
+                patcher.fs.create_file("/report/jscpd-report.json", contents=report.read())
+                plugin = DangerJSCPD()
+                plugin.jscpd()
 
     assert not danger.results.fails
