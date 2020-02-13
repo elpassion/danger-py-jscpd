d4891d10f31576b23f472e8f281a5fc2b3abeb31
Mateusz Szklarek <mateusz.szklarek@gmail.com>
2020-02-13 12:15:54 +0100

    Add first test for DangerJSCPD

--- danger_py_jscpd/plugin.py
+++ danger_py_jscpd/plugin.py
@@ -2,5 +2,4 @@ from danger_python.plugins import DangerPlugin
 
 
 class DangerJSCPD(DangerPlugin):
-    def jscpd(self):
-        self.fail("test_message")
+    pass
--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,10 +1,8 @@
 from danger_python.danger import Danger, Violation
+from danger_python.plugins import DangerPlugin
 
 from danger_py_jscpd import DangerJSCPD
 
 
-def test_plugin_does_fail_with_test_message(danger: Danger):
-    plugin = DangerJSCPD()
-    plugin.jscpd()
-
-    assert danger.results.fails == [Violation(message="test_message")]
+def test_plugin_inherits_from_danger_plugin(danger: Danger):
+    assert issubclass(DangerJSCPD, DangerPlugin)
