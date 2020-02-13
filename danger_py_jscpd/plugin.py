a2c72365b3a5e1265d7f9e083efd9dd365155836
Mateusz Szklarek <mateusz.szklarek@gmail.com>
2020-02-13 12:58:49 +0100

    Implement checking jscpd

--- danger_py_jscpd/plugin.py
+++ danger_py_jscpd/plugin.py
@@ -1,5 +1,10 @@
+import subprocess
+
 from danger_python.plugins import DangerPlugin
 
 
 class DangerJSCPD(DangerPlugin):
-    pass
+    def jscpd(self):
+        result = subprocess.run(["which", "jscpd"], capture_output=True, text=True)
+        if result.returncode == 1:
+            self.fail("Could not find jscpd in current directory, pleas run command `npm install -g jscpd`")
--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,8 +1,32 @@
+from unittest.mock import patch
+
 from danger_python.danger import Danger, Violation
 from danger_python.plugins import DangerPlugin
+from testfixtures.popen import MockPopen
 
 from danger_py_jscpd import DangerJSCPD
 
 
 def test_plugin_inherits_from_danger_plugin():
     assert issubclass(DangerJSCPD, DangerPlugin)
+
+
+def test_plugin_does_fail_when_jscpd_not_installed(danger: Danger):
+    with patch("subprocess.Popen", new_callable=MockPopen) as popen:
+        popen.set_command("which jscpd", returncode=1)
+
+        plugin = DangerJSCPD()
+        plugin.jscpd()
+
+    message = "Could not find jscpd in current directory, pleas run command `npm install -g jscpd`"
+    assert danger.results.fails == [Violation(message=message)]
+
+
+def test_plugin_does_pass_when_jscpd_installed(danger: Danger):
+    with patch("subprocess.Popen", new_callable=MockPopen) as popen:
+        popen.set_command("which jscpd", returncode=0)
+
+        plugin = DangerJSCPD()
+        plugin.jscpd()
+
+    assert not danger.results.fails
